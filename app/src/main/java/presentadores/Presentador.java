4cd54ab304236a2085aee857b27c4848b5d541bf
Jose <jose@droiders.com>
2015-05-06 19:37:52 +0200

    nuevas features

---
+++ app/src/main/java/presentadores/Presentador.java
@@ -0,0 +1,12 @@
+package presentadores;
+
+/**
+ * Created by jose on 6/05/15.
+ */
+public class Presentador {
+
+    public void change(){
+        int i;
+        i = 0;
+    }
+}
--- app/src/main/java/umu/git/MainActivity.java
+++ app/src/main/java/umu/git/MainActivity.java
@@ -5,13 +5,18 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import presentadores.Presentador;
+
 
 public class MainActivity extends ActionBarActivity {
 
+    Presentador p;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        p = new Presentador();
+        p.change();
     }
 
     @Override
